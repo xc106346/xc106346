2be5102fec4112d9e14503a43eef0e9a90df858a
xc106346 <91717031+xc106346@users.noreply.github.com>
2025-03-25 17:00:50 +0800

    Update cloudmusic.js

--- cloudmusic.js
+++ cloudmusic.js
@@ -4,232 +4,202 @@ const headers = {
   'User-Agent': UA,
 };
 
-const appConfig = {
-  "ver": 1,
-  "name": "猫耳FM",
-  "message": "",
-  "desc": "",
-  "tabLibrary": {
-    "name": "探索",
-    "groups": [{
-        "name": "推荐",
-        "type": "song",
-        "ui": 0,
-        "showMore": true,
-        "ext": {
-            "id": '1'
-        }
-    }, {
-        "name": "音单",
-        "type": "album",
-        "ui": 0,
-        "showMore": true,
-        "ext": {
-            "id": '2'
-        }
-    }, {
-        "name": "歌单",
-        "type": "playlist",
-        "ui": 1,
-        "showMore": true,
-        "ext": {
-            "id": '3'
-        }
-    }, {
-      "name": "排行榜",
-      "type": "playlist",
-      "ui": 1,
-      "showMore": true,
-      "ext": {
-          "id": '4'
-      }
-    }]
-  },
-  "tabMe": {
-    "name": "我的",
-    "groups": [{
-      "name": "歌曲",
-      "type": "song"
-    }]
-  },
-  "tabSearch": {
-    "name": "搜索",
-    "groups": [{
-      "name": "歌曲",
-      "type": "song",
-      "ext": {
-        "type": "song"
-      }
-    }]
-  }
-};
-
-const cache = {};
-
-const getCacheData = (key) => cache[key];
-
-const setCacheData = (key, data, expirationTime = 3600) => {
-  cache[key] = { data, timestamp: Date.now() };
-  setTimeout(() => delete cache[key], expirationTime * 1000); // Expire cache after the specified time
-};
-
-const fetchData = async (url, headers) => {
-  try {
-    const { data } = await $fetch.get(url, { headers });
-    return argsify(data);
-  } catch (error) {
-    console.error(`Error fetching data from ${url}:`, error);
-    return null;
-  }
-};
-
-async function getPaginatedData(url, page = 1, pageSize = 20) {
-  try {
-    const { data } = await $fetch.get(`${url}?page=${page}&pageSize=${pageSize}`, { headers });
-    return argsify(data);
-  } catch (error) {
-    console.error(`Error fetching paginated data from ${url}:`, error);
-    return null;
-  }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = require("axios");
+const pageSize = 30;
+function validMusicFilter(_) {
+    return `${_.pay_type}` === "0";
 }
-
-async function getMultipleData(urls) {
-  try {
-    const promises = urls.map(url => $fetch.get(url, { headers }));
-    const responses = await Promise.all(promises);
-    return responses.map(response => argsify(response.data));
-  } catch (error) {
-    console.error(`Error fetching multiple data:`, error);
-    return [];
-  }
+function formatMusicItem(_) {
+    var _a;
+    return {
+        id: _.id,
+        artwork: _.front_cover,
+        title: _.soundstr,
+        artist: _.username,
+        user_id: _.user_id,
+        duration: +((_a = _.duration) !== null && _a !== void 0 ? _a : 0),
+    };
 }
-
-async function getSongs(ext) {
-  const { page, id } = argsify(ext);
-  const songs = [];
-  if (page > 1) return jsonify({ list: [] });
-
-  const data = await getPaginatedData('https://www.missevan.com/sound/newhomepagedata', page);
-  if (data) {
-    const songList = data.info.music.flatMap(genre =>
-      genre.objects_point.map(each => ({
-        id: `${each.id}`,
-        name: each.soundstr,
-        cover: each.front_cover,
-        duration: parseInt(each.duration / 100),
-        artist: {
-          id: `${each.user_id}`,
-          name: each.username
+function formatAlbumItem(_) {
+    return {
+        id: _.id,
+        artist: _.author,
+        title: _.name,
+        artwork: _.cover,
+        description: _.abstract,
+    };
+}
+const searchHeaders = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+    accept: "application/json",
+    "accept-encoding": "gzip, deflate, br",
+    referer: "https://www.missevan.com/sound/search",
+};
+async function searchMusic(query, page) {
+    const res = (await axios_1.default.get("https://www.missevan.com/sound/getsearch", {
+        params: {
+            s: query,
+            p: page,
+            type: 3,
+            page_size: pageSize,
         },
-        ext: {
-          id: each.id
-        }
-      }))
-    );
-    songs.push(...songList);
-  }
-
-  return jsonify({ list: songs });
+        headers: searchHeaders,
+    })).data.info;
+    return {
+        isEnd: res.pagination.p >= res.pagination.maxpage,
+        data: res.Datas.filter(validMusicFilter).map(formatMusicItem),
+    };
 }
-
-async function getArtists(ext) {
-  const { page, id, from } = argsify(ext);
-  return jsonify({
-    list: [],
-  });
-}
-
-async function getPlaylists(ext) {
-  const { page, id } = argsify(ext);
-  if (page > 1) return jsonify({ list: [] });
-
-  let cards = [];
-  if (id == '3') {
-    const urls = ['https://www.missevan.com/explore/tagalbum?order=0'];
-    const responses = await getMultipleData(urls);
-    responses.forEach(response => {
-      response.albums.forEach(each => {
-        cards.push({
-          id: `${each.id}`,
-          name: each.title,
-          cover: each.front_cover,
-          artist: {
-            id: `${each.user_id}`,
-            name: each.username
-          },
-          ext: {
-            id: each.id
-          }
-        });
-      });
-    });
-  }
-
-  return jsonify({ list: cards });
-}
-
-async function getSongInfo(ext) {
-  const { url, id, qid } = argsify(ext);
-  if (url) {
-    const cachedSound = getCacheData(url);
-    if (cachedSound) return jsonify({ urls: [cachedSound] });
-
-    const data = await fetchData(url, headers);
-    if (data) {
-      setCacheData(url, data);
-      return jsonify({ urls: [data] });
-    }
-  }
-
-  if (id) {
-    const data = await fetchData(`https://www.missevan.com/sound/getsound?soundid=${id}`, headers);
-    if (data) {
-      const soundUrl = argsify(data).info.sound.soundurl;
-      setCacheData(id, soundUrl);
-      return jsonify({ urls: [soundUrl], headers: [{ 'User-Agent': UA }] });
-    }
-  }
-
-  if (qid) {
-    const data = await fetchData(`https://lxmusicapi.onrender.com/url/tx/${qid}/320k`, { 'X-Request-Key': 'share-v2' });
-    if (data) {
-      const soundUrl = argsify(data).url;
-      setCacheData(qid, soundUrl);
-      return jsonify({ urls: [soundUrl] });
-    }
-  }
-
-  return jsonify({ urls: [] });
-}
-
-async function search(ext) {
-  const { text, page, type } = argsify(ext);
-  if (page > 1) return jsonify({});
-
-  if (type == 'song') {
-    const songs = [];
-    const { data } = await $fetch.get(`http://c.y.qq.com/soso/fcgi-bin/client_search_cp?new_json=1&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w=${encodeURIComponent(text)}&needNewCode=0`, { headers });
-
-    argsify(data.slice(9, -1)).data?.song?.list?.forEach(each => {
-      songs.push({
-        id: `${each.mid}`,
-        name: each.name,
-        cover: `https://y.gtimg.cn/music/photo_new/T002R800x800M000${each.album.mid}.jpg`,
-        duration: 0,
-        artist: {
-          id: `${each.singer[0]?.id}`,
-          name: each.singer[0]?.name || '',
+async function searchAlbum(query, page) {
+    const res = (await axios_1.default.get("https://www.missevan.com/dramaapi/search", {
+        headers: searchHeaders,
+        params: {
+            s: query,
+            page,
         },
-        ext: {
-          qid: each.mid
-        }
-      });
-    });
-
-    return jsonify({
-      list: songs,
-    });
-  }
-
-  return jsonify({});
+    })).data.info;
+    return {
+        isEnd: res.pagination.p >= res.pagination.maxpage,
+        data: res.Datas.filter(validMusicFilter).map(formatAlbumItem),
+    };
 }
+async function getAlbumInfo(albumItem) {
+    const res = (await axios_1.default.get("https://www.missevan.com/dramaapi/getdrama", {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+            accept: "application/json",
+            "accept-encoding": "gzip, deflate, br",
+            referer: `https://www.missevan.com/mdrama/${albumItem.id}`,
+        },
+        params: {
+            drama_id: albumItem.id,
+        },
+    })).data;
+    return {
+        musicList: res.info.episodes.episode
+            .filter(validMusicFilter)
+            .map(_ => {
+            const r = formatMusicItem(_);
+            r.artwork = albumItem.artwork;
+            return r;
+        }),
+    };
+}
+async function getMediaSource(musicItem, quality) {
+    if (quality === "high" || quality === "super") {
+        return;
+    }
+    const res = (await axios_1.default.get("https://www.missevan.com/sound/getsound", {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+            accept: "application/json",
+            "accept-encoding": "gzip, deflate, br",
+            referer: `https://www.missevan.com/sound/player?id=${musicItem.id}`,
+        },
+        params: {
+            soundid: musicItem.id,
+        },
+    })).data.info;
+    if (quality === "low") {
+        return {
+            url: res.sound.soundurl_128,
+        };
+    }
+    else {
+        return {
+            url: res.sound.soundurl,
+        };
+    }
+}
+async function getRecommendSheetTags() {
+    const res = (await axios_1.default.get(`https://www.missevan.com/malbum/recommand`, {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+            accept: "application/json",
+            "accept-encoding": "gzip, deflate, br",
+            referer: `https://www.missevan.com`,
+        }
+    })).data.info;
+    const data = Object.entries(res !== null && res !== void 0 ? res : {}).map(group => ({
+        title: group[0],
+        data: group[1].map(_ => ({
+            id: _[0],
+            title: _[1]
+        }))
+    }));
+    return {
+        data,
+    };
+}
+async function getRecommendSheetsByTag(tag, page) {
+    const res = (await axios_1.default.get(`https://www.missevan.com/explore/tagalbum`, {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+            accept: "application/json",
+            "accept-encoding": "gzip, deflate, br",
+            referer: `https://m.missevan.com`,
+        },
+        params: {
+            order: 0,
+            tid: (tag === null || tag === void 0 ? void 0 : tag.id) || 0,
+            p: page
+        }
+    })).data;
+    return {
+        isEnd: res.page >= res.maxpage,
+        data: res.albums.map(sheet => ({
+            id: sheet.id,
+            title: sheet.title,
+            artwork: sheet.front_cover,
+            artist: sheet.username,
+            createUserId: sheet.user_id
+        }))
+    };
+}
+async function getMusicSheetInfo(sheet, page) {
+    const res = (await axios_1.default.get(`https://www.missevan.com/sound/soundalllist`, {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.61",
+            accept: "application/json",
+            "accept-encoding": "gzip, deflate, br",
+            referer: `https://m.missevan.com`,
+        },
+        params: {
+            albumid: sheet.id
+        }
+    })).data.info;
+    return {
+        isEnd: true,
+        musicList: res.sounds.filter(validMusicFilter).map(item => ({
+            id: item.id,
+            title: item.soundstr,
+            artwork: item.front_cover,
+            url: item.soundurl,
+            artist: item.username,
+        }))
+    };
+}
+module.exports = {
+    platform: "猫耳FM",
+    author: '猫头猫',
+    version: "0.1.4",
+    appVersion: ">0.1.0-alpha.0",
+    srcUrl: "https://gitee.com/maotoumao/MusicFreePlugins/raw/v0.1/dist/maoerfm/index.js",
+    cacheControl: "no-cache",
+    supportedSearchType: ["music", "album",],
+    async search(query, page, type) {
+        if (type === "music") {
+            return await searchMusic(query, page);
+        }
+        if (type === "album") {
+            return await searchAlbum(query, page);
+        }
+    },
+    getMediaSource,
+    getAlbumInfo,
+    getRecommendSheetTags,
+    getRecommendSheetsByTag,
+    getMusicSheetInfo
+};
